fd4bbf852d3f3b8bf59bd43b6fb2f8ad50be21ac
Evgeniy Pavlichenko <yevheniipavl@gmail.com>
2022-11-22 20:38:24 +0200

    without vanta

--- src/index.js
+++ src/index.js
@@ -66,16 +66,16 @@ function onClearPage() {
   countryList.innerHTML = '';
 }
 
-VANTA.WAVES({
-  el: '#waves',
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.0,
-  minWidth: 200.0,
-  scale: 1.0,
-  scaleMobile: 1.0,
-  waveHeight: 11.0,
-  zoom: 0.7,
-  color: 0x344d,
-});
+// VANTA.WAVES({
+//   el: '#waves',
+//   mouseControls: true,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 200.0,
+//   minWidth: 200.0,
+//   scale: 1.0,
+//   scaleMobile: 1.0,
+//   waveHeight: 11.0,
+//   zoom: 0.7,
+//   color: 0x344d,
+// });
