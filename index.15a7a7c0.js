91cd96302c76a256e27f148d66142b3c782316e7
JeK566787 <92581680+JeK566787@users.noreply.github.com>
2022-11-22 14:20:55 +0000

    Deploying to gh-pages from @ JeK566787/goit-js-hw-10@f0c87b6780e2476b155dec723e26dfefeabf0c32 🚀

---
+++ index.15a7a7c0.js
@@ -0,0 +1,2 @@
+VANTA.WAVES({el:"#waves",mouseControls:!0,touchControls:!0,gyroControls:!1,minHeight:200,minWidth:200,scale:1,scaleMobile:1,waveHeight:11,zoom:.7,color:13389});
+//# sourceMappingURL=index.15a7a7c0.js.map
