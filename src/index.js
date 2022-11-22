5e69f31229353ffc8afe432017b1e81808bc54be
Evgeniy Pavlichenko <yevheniipavl@gmail.com>
2022-11-22 17:11:21 +0200

    new

--- src/index.js
+++ src/index.js
@@ -65,3 +65,17 @@ function onClearPage() {
   countryInfo.innerHTML = '';
   countryList.innerHTML = '';
 }
+
+VANTA.WAVES({
+  el: '#waves',
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  waveHeight: 11.0,
+  zoom: 0.7,
+  color: 0x344d,
+});
